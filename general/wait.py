14fda52c30116906ddb1a0e3fe2e28ea4b356d58
lanting0119 <813324313@qq.com>
2025-03-04 19:56:06 +0800

    add wait.py

---
+++ general/wait.py
@@ -0,0 +1,35 @@
+#进入待分类文件夹
+import os,shutil
+import binpacking
+import sys,time,pyautogui,os
+import tkinter 
+from tkinter.messagebox import *
+import time
+from natsort import natsorted
+import sys
+
+def sleep(sleeptime):
+
+    time.sleep(2)
+
+    for i in range(int(sleeptime/4)):
+        print(i)
+        pyautogui.click(x=1000, y=600)
+        time.sleep(3)
+        pyautogui.click(x=1000, y=800)
+        time.sleep(3)
+
+
+        # pyautogui.click(x=1400, y=400)
+        # time.sleep(30)
+        # pyautogui.click(x=1400, y=200)
+        # time.sleep(30)
+        # pyautogui.typewrite('y')
+        # time.sleep(5)
+        # pyautogui.press('enter')
+        # time.sleep(5)
+
+
+sleep(10000)
+
+print('hello')
